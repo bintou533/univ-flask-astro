3c90c7901b492aef9594ef0349cd0924609be3f4
bintou533 <bintou.ndiaye@etu.univ-smb.fr>
2025-04-09 18:57:53 +0000

    bintou

--- app.py
+++ app.py
@@ -1,87 +1,30 @@
-from flask import Flask, render_template, redirect, url_for, request
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
-from flask_bcrypt import Bcrypt
+from flask import Flask, render_template
 
-# Initialisation des extensions
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'bintou'  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Base de données SQLite
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Initialiser l'extension SQLAlchemy et Bcrypt
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-
-
-
-# Fonction de chargement de l'utilisateur
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-# Route pour la page d'accueil
 @app.route('/')
-@login_required
-def home():
-    return render_template('index.html', title="Accueil")
+def index():
+    return render_template('index.html')
 
-# Route pour la page d'inscription
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        email = request.form['email']
-        password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
-        
-        user = User(username=username, email=email, password=password)
-        db.session.add(user)
-        db.session.commit()
-        
-        login_user(user)
-        return redirect(url_for('home'))
-    
-    return render_template('register.html', title='Inscription')
-
-# Route pour la page de connexion
-
-@app.route('/login', methods=['GET', 'POST'])
-
-def login():
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        user = User.query.filter_by(email=email).first()
-        
-        if user and bcrypt.check_password_hash(user.password, password):
-            login_user(user)
-            next_page = request.args.get('next')  
-            return redirect(next_page or url_for('home'))  
-    
-    return render_template('login.html', title='Connexion')
-
-# Route pour la déconnexion
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
-
-
-@app.route('/appareil-photo')
-@login_required
+@app.route('/appareil_photo')
 def appareil_photo():
-    return render_template('appareil_photo.html', appareils=photos)
+    return render_template('appareil_photo.html')
 
 @app.route('/telescope')
-@login_required
 def telescope():
-    return render_template('telescope.html', teles=teles)
+    return render_template('telescope.html')
 
 @app.route('/photographies')
-@login_required
 def photographies():
-    return render_template('photographies.html', photos=photographies)
+    return render_template('photographies.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
